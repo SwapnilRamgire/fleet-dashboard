30a0955f0bc1293baf3d227f8f175aff5668c928
SwapnilRamgire <swapnilramgire007@gmail.com>
2025-09-27 15:16:23 +0530

    shadcn and tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button"
+import { cn } from "./lib/utils"
+
 function App() {
   return (
     <div>
       Hello There
+      <Button>Button Here</Button>
     </div>
   )
 }
